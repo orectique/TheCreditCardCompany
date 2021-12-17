ae910177df2d632684f23a4c1a500ba792447146
Sreehari Pulickamadhom Sreedhar <49713741+orectique@users.noreply.github.com>
2021-12-17 17:17:30 +0530

    changes

---
+++ pages/generate.js
@@ -0,0 +1,4 @@
+document.getElementById("Name").innerHTML = 'Bob';
+document.getElementById("ccNum").innerHTML = '1234-1234-1234-1345';
+document.getElementById("cvv").innerHTML = '345';
+document.getElementById("exp").innerHTML = '11/19';
